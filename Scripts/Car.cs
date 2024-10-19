ee424f450661a60a03ab34e1ba05a37aff0d06ca
Aruzhan Ismagulova <aruchanism@gmail.com>
2024-10-19 11:03:37 +0500

    added script car.cs

--- Scripts/Car.cs
+++ Scripts/Car.cs
@@ -3,41 +3,74 @@ using System;
 
 public partial class Car : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	[Export] private Node casts;
+	private int _rayNum;
+	private float[] _interest;
+	private float[] _danger;
+	private Vector3 _chosenDir = Vector3.Zero;
 
-	public override void _PhysicsProcess(double delta)
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
 	{
-		Vector3 velocity = Velocity;
+		_rayNum = casts.GetChildCount();
+		_interest = new float[_rayNum];
+		_interest = new float[_rayNum];
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.	public override void _Process(double delta)
 
-		// Add the gravity.
-		if (!IsOnFloor())
+	public Vector3 GetDir(int num)
+	{
+		RayCast3D cast = (RayCast3D)casts.GetChild(num);
+		return -cast.GlobalTransform.Basis.Z;
+	}
+
+	public Vector3 CalculatePath(Vector3 pos)
+	{
+		var dir = GlobalTransform.Origin.DirectionTo(pos);
+		_interest = new float[_rayNum];
+		_danger = new float[_rayNum];
+		SetInterest(dir);
+		SetDanger();
+		var newDir = DecidePath();
+		return newDir;
+	}
+
+	public void SetInterest(Vector3 preferredDirection)
+	{
+		for (int i = 0; i < _rayNum; i++)
 		{
-			velocity += GetGravity() * (float)delta;
+			var d = GetDir(i).Dot(preferredDirection);
+			_interest[i] = Math.Max(0, d);
+		}
+	}
+
+	public void SetDanger()
+	{
+		for (int i = 0; i < _rayNum; i++)
+		{
+			var cast = (RayCast3D)casts.GetChild(i);
+			var result = cast.IsColliding();
+			_danger[i] = result ? 1.0f : 0.0f;
+		}
+	}
+
+	public Vector3 DecidePath()
+	{
+		for (int i = 0; i < _rayNum; i++)
+		{
+			if (_danger[i] > 0.0f)
+			{
+				_interest[i] = 0;
+			}
 		}
 
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		var dir = Vector3.Zero;
+		for (int i = 0; i < _rayNum; i++)
 		{
-			velocity.Y = JumpVelocity;
+			dir += GetDir(i) * _interest[i];
 		}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
-
-		Velocity = velocity;
-		MoveAndSlide();
+		dir = dir.Normalized();
+		return dir;
 	}
 }
